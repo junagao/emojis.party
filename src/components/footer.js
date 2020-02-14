cbfc00122aea033d33db7309d46e4292d9f4f46e
Juliane Nagao <julianens@gmail.com>
2020-02-14 13:44:01 +0100

    feat: footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
 
-const Footer = () => <footer>Footer</footer>
+const Footer = ({ siteAuthor, siteGithub }) => (
+  <footer className="footer">
+    <div className="made-with-love">
+      made with{" "}
+      <span role="img" aria-label="heart">
+        ❤️
+      </span>{" "}
+      by <a href="http://junagao.com">{siteAuthor}</a>
+    </div>
+    <nav className="footer-nav">
+      <Link href="/about">about</Link>
+      <span className="divider">|</span>
+      <a href={siteGithub}>github</a>
+    </nav>
+  </footer>
+)
+
+Footer.propTypes = {
+  siteAuthor: PropTypes.string.isRequired,
+  siteGithub: PropTypes.string.isRequired,
+}
 
 export default Footer
