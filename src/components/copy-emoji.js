a674c0409e4c983209186c2492f21dcf2936d89b
Juliane Nagao <julianens@gmail.com>
2020-02-17 21:11:01 +0100

    feat: style copy-emoji component

--- src/components/copy-emoji.js
+++ src/components/copy-emoji.js
@@ -1,12 +1,49 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled, { keyframes } from "styled-components"
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+
+const StyledCopyContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  top: 0;
+  left: 0;
+  position: fixed;
+  z-index: 10;
+  animation: ${fadeIn} 0.25s ease-in;
+`
+
+const StyledCopied = styled.div`
+  font-size: 32px;
+  white-space: nowrap;
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 50%;
+  background-color: rgba(255, 255, 255, 0.95);
+  color: rgb(233, 60, 60);
+  font-weight: 600;
+  padding: 32px 40px;
+  text-align: center;
+  border-radius: 70px;
+  z-index: 2;
+`
 
 const CopyEmoji = ({ copySuccess, copiedValue }) =>
-  copySuccess ? (
-    <div className="copy-wrap">
-      <div className="copied">copied {copiedValue}</div>
-    </div>
-  ) : null
+  copySuccess && (
+    <StyledCopyContainer>
+      <StyledCopied>copied {copiedValue}</StyledCopied>
+    </StyledCopyContainer>
+  )
 
 CopyEmoji.propTypes = {
   copiedValue: PropTypes.string.isRequired,
