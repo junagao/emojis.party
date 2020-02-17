c57517ec3a4c5b9e7de8ddc71c7056338fd4f19c
Juliane Nagao <julianens@gmail.com>
2020-02-17 21:13:07 +0100

    feat: style SearchResults component

--- src/components/search-results.js
+++ src/components/search-results.js
@@ -1,19 +1,68 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled, { keyframes } from "styled-components"
+
+const emojisFadeIn = keyframes`
+  from {
+    left: 0;
+    padding-top: 100px;
+    opacity: 0;
+  }
+  to {
+    left: 0;
+    top: 0;
+    opacity: 1;
+  }
+`
+
+const StyleResultsContainer = styled.div`
+  padding: 0px 64px 120px 64px;
+  animation: ${emojisFadeIn} 0.25s ease-in;
+  -moz-animation: ${emojisFadeIn} 0.25s ease-in; /* Firefox */
+  -webkit-animation: ${emojisFadeIn} 0.25s ease-in; /* Safari and Chrome */
+  -o-animation: ${emojisFadeIn} 0.25s ease-in; /* Opera */
+`
+
+const StyledResultsList = styled.ul`
+  justify-content: space-evenly;
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const StyledResultsListItem = styled.li``
+
+const StyledEmojiButton = styled.button`
+  font-size: 88px;
+  padding: 32px 38px;
+  transition: all 0.25s ease-out;
+  cursor: pointer;
+  background: transparent;
+  border: 0;
+  outline: none;
+  border-radius: 4px;
+  :active {
+    background-color: rgba(233, 60, 60, 0.25);
+    transition: 1s;
+  }
+`
 
 const SearchResults = ({ searchTerm, searchResults, onCopyToClipboard }) =>
   searchTerm && (
-    <div className="results">
-      <ul className="results-list">
+    <StyleResultsContainer>
+      <StyledResultsList>
         {searchResults.map(emoji => (
-          <li key={emoji.unicode}>
-            <button type="button" onClick={() => onCopyToClipboard(emoji.char)}>
+          <StyledResultsListItem key={emoji.unicode}>
+            <StyledEmojiButton
+              type="button"
+              onClick={() => onCopyToClipboard(emoji.char)}
+            >
               {emoji.char}
-            </button>
-          </li>
+            </StyledEmojiButton>
+          </StyledResultsListItem>
         ))}
-      </ul>
-    </div>
+      </StyledResultsList>
+    </StyleResultsContainer>
   )
 
 SearchResults.propTypes = {
