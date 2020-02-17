9849efcb21c4e8104bef35b05c9f92a38318ee9b
Juliane Nagao <julianens@gmail.com>
2020-02-17 21:12:31 +0100

    feat: style search-input component

--- src/components/search-input.js
+++ src/components/search-input.js
@@ -1,16 +1,47 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled from "styled-components"
+
+const StyledInputContainer = styled.div`
+  flex-basis: 80%;
+  z-index: 1;
+`
+
+const StyledInput = styled.input`
+  padding: 12px 32px 16px 32px;
+  border: 1px solid transparent;
+  border-radius: 54px;
+  color: rgb(233, 60, 60);
+  background-color: rgba(233, 60, 60, 0.1);
+  font-weight: 700;
+  font-size: 60px;
+  outline: none;
+  width: 100%;
+  box-sizing: border-box;
+  z-index: 1;
+  :focus {
+    border: 1px solid rgb(233, 60, 60, 0.4);
+
+    background-color: #fff;
+  }
+  ::placeholder {
+    color: rgba(233, 60, 60, 0.5);
+  }
+  :hover {
+    box-shadow: 2px 3px 0px 0px rgba(233, 60, 60, 0.5);
+  }
+`
 
 const SearchInput = ({ searchTerm, onTermChange }) => (
-  <div className="search-input">
-    <input
+  <StyledInputContainer>
+    <StyledInput
       id="searchInput"
       type="text"
       value={searchTerm}
       onChange={onTermChange}
-      placeholder="search emoji"
+      placeholder="search"
     />
-  </div>
+  </StyledInputContainer>
 )
 
 SearchInput.propTypes = {
